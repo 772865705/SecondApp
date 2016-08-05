24fa5d0d343bb181270502ebac1e1a7440d70f4a
772865705 <772865705@qq.com>
2016-08-05 16:47:02 +0800

    添加了一个String

--- app/src/main/java/com/zy/secondapp/MapsActivity.java
+++ app/src/main/java/com/zy/secondapp/MapsActivity.java
@@ -10,9 +10,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+/**
+ *
+ */
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private String string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
